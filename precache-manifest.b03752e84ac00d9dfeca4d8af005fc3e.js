4279bcb8ca5c232e0da44b78047f791fd7e6cf35
Murtaza <murtaala@amazon.com>
2020-04-16 19:57:58 +0530

    gradient background

---
+++ precache-manifest.b03752e84ac00d9dfeca4d8af005fc3e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f318fa7062e633f140da67db7278737",
+    "url": "/index.html"
+  },
+  {
+    "revision": "013139339d6d3b13487c",
+    "url": "/static/css/main.6b1e656f.chunk.css"
+  },
+  {
+    "revision": "19b37efddf6f75ec3a87",
+    "url": "/static/js/2.351a2456.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/static/js/2.351a2456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "013139339d6d3b13487c",
+    "url": "/static/js/main.8b60b5a3.chunk.js"
+  },
+  {
+    "revision": "b2a9922fccf3ed3bdd4b",
+    "url": "/static/js/runtime-main.4e66121f.js"
+  },
+  {
+    "revision": "662a4f1472618eb2eb3b507d7459741f",
+    "url": "/static/media/instagram.662a4f14.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f3987ec629fbf8e7084f9c91c2f2e4f8",
+    "url": "/static/media/size-logo.f3987ec6.png"
+  },
+  {
+    "revision": "be06689853aec102607f5e54e6fd2dfa",
+    "url": "/static/media/twitter.be066898.svg"
+  },
+  {
+    "revision": "15a5f3d81716df62006ba91e6df99a6c",
+    "url": "/static/media/whatsapp.15a5f3d8.svg"
+  },
+  {
+    "revision": "d57a47e161639189da5621dd9cef0c1f",
+    "url": "/static/media/youtube.d57a47e1.svg"
+  }
+]);
